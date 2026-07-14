ce38f59ee984d8f76b7939fd0924dd3ebdbdcbb6
janaSk <janaththan@codelantic.com>
2026-07-14 13:17:20 +0530

    backend project setup

---
+++ src/test/java/smart/service/system/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package smart.service.system.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
